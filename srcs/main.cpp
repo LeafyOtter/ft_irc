52779f70ac66a111329ae08ebca04a6c8360d650
loutre <loutre@courrier.dev>
2022-08-18 21:21:05 +0200

    [main] : added a temporary main to test the current files

---
+++ srcs/main.cpp
@@ -0,0 +1,28 @@
+#include "c_irc.hpp"
+#include "Server.hpp"
+
+#include <string>
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		std::cout << "Usage: ./server <ip> <port>" << std::endl;
+		return (0);
+	}
+
+	c_irc::Server server;
+
+	try
+	{
+		server.initialize("c_irc.net", av[1], av[2]);
+		server.start();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+		return (1);
+	}
+	return (0);
+}
