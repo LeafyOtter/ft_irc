f66d3263418005aef21bdc482bf699cc07cbc52f
loutre <loutre@courrier.dev>
2022-08-19 13:01:50 +0200

    [Message] : refactored how a target is handled. added copy constructor and copy operator

--- srcs/Message.cpp
+++ srcs/Message.cpp
@@ -2,30 +2,76 @@
 
 namespace c_irc
 {
-	Message::Message(users_it first, users_it last)
-		: message("")
-		, first(first)
-		, last(last)
-		, to_pop(false) {}
+	Message::Message(users_it_t first, users_it_t last)
+		: to_pop(false)
+		, message(std::string())
+		, target_type(TARGET_RANGE)
+		, target(NULL)
+		, first_target(first)
+		, last_target(last)
+		, sender(users_it_t())
+	{}
 
-	Message::Message(std::string msg, users_it first, users_it last)
-		: message(msg)
-		, first(first)
-		, last(last)
-		, to_pop(false) {}
+	Message::Message(std::string msg, users_it_t first, users_it_t last)
+		: to_pop(false)
+		, message(msg)
+		, target_type(TARGET_RANGE)
+		, target(NULL)
+		, first_target(first)
+		, last_target(last)
+		, sender(users_it_t())
+	{}
+
+	Message::Message(std::string msg, pollfd *new_pfd)
+		: to_pop(false)
+		, message(msg)
+		, target_type(TARGET_UNREGISTERED)
+		, target(new_pfd)
+		, first_target(users_it_t())
+		, last_target(users_it_t())
+		, sender(users_it_t())
+	{}
+
+	Message::Message(const Message &other)
+		: to_pop(other.to_pop)
+		, message(other.message)
+		, target_type(other.target_type)
+		, target(other.target)
+		, first_target(other.first_target)
+		, last_target(other.last_target)
+		, sender(other.sender)
+	{}
 
 	Message::~Message() {}
+
+	Message &Message::operator=(const Message &other)
+	{
+		if (this != &other)
+		{
+			to_pop = other.to_pop;
+			message = other.message;
+			target_type = other.target_type;
+			target = other.target;
+			first_target = other.first_target;
+			last_target = other.last_target;
+			sender = other.sender;
+		}
+		return (*this);
+	}
 
 	std::string Message::get_message() const { return (message); }
 	void Message::set_status() { to_pop = true; }
 	bool Message::get_status() { return (to_pop); }
 	void Message::set_message(std::string new_message) { message = new_message; }
-	void Message::set_sender(users_it new_sender) { sender = new_sender; }
+	void Message::set_sender(users_it_t new_sender) { sender = new_sender; }
 
 	int Message::nb_users() const {
+
 		int i = 0;
 
-		for (users_it it = first; it != last; ++it) {
+		if (target_type == TARGET_UNREGISTERED)
+			return (1);
+		for (users_it_t it = first_target; it != last_target; ++it) {
 			if (it != sender)
 				++i;
 		}
@@ -33,10 +79,13 @@ namespace c_irc
 	}
 
 	void	Message::prepare() {
-		for (users_it it = first; it != last; ++it) {
+		if (target_type == TARGET_UNREGISTERED) {
+			target->events |= POLLOUT;
+			return ;
+		}
+		for (users_it_t it = first_target; it != last_target; ++it) {
 			if (it != sender)
-				(*it)->set_pollout();
+				(*it).first->set_pollout();
 		}
 	}
-
 } // namespace c_irc
--- srcs/Message.hpp
+++ srcs/Message.hpp
@@ -11,21 +11,30 @@ namespace c_irc
 	{
 		private:	
 
-		typedef typename users_set_t::iterator users_it;
+		typedef enum target_type_e {
+			TARGET_UNREGISTERED,
+			TARGET_RANGE
+		} target_type_t;
 
-		std::string		message;
-		users_it		first;
-		users_it		last;
-		users_it		sender;
 		bool			to_pop;
+		std::string		message;
+		target_type_t	target_type;
+		pollfd			*target;
+		users_it_t		first_target;
+		users_it_t		last_target;
+		users_it_t		sender;
 
 		Message();
 
 		public:
 
-		Message(users_it first, users_it last);
-		Message(std::string msg, users_it first, users_it last);
+		Message(users_it_t first, users_it_t last);
+		Message(std::string msg, users_it_t first, users_it_t last);
+		Message(std::string msg, pollfd *new_pfd);
+		Message(const Message &other);
 		~Message();
+
+		Message &operator=(const Message &other);
 
 		std::string		get_message() const;
 
@@ -34,7 +43,7 @@ namespace c_irc
 
 		int				nb_users() const;
 
-		void			set_sender(users_it new_sender);
+		void			set_sender(users_it_t new_sender);
 
 		void			set_message(std::string new_message);
 
