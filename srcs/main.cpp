e834c2a063087e38eaba7ad5a3792b8d3fdbc3d2
loutre <loutre@courrier.dev>
2022-08-23 16:43:20 +0200

    [main] : uncommented functions

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -14,18 +14,15 @@ int main(int ac, char **av)
 
 	c_irc::Server server;
 
+	try
+	{
 		server.initialize("c_irc.net", av[1], av[2]);
 		server.start();
-
-	// try
-	// {
-	// 	server.initialize("c_irc.net", av[1], av[2]);
-	// 	server.start();
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << '\n';
-	// 	return (1);
-	// }
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+		return (1);
+	}
 	return (0);
 }
