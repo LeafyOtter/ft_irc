1643d31715c31226db1cca249ccd494025826912
loutre <loutre@courrier.dev>
2022-08-25 13:09:02 +0200

    [pass] : finished implementation

--- srcs/commands/user/pass.cpp
+++ srcs/commands/user/pass.cpp
@@ -2,6 +2,8 @@
 #include "../errors.hpp"
 #include "../replies.hpp"
 
+#include <iostream>
+
 namespace c_irc
 {
 	// Possible errors :
@@ -11,27 +13,34 @@ namespace c_irc
 	void	Server::cmd_pass(int fd, arguments_t &args)
 	{
 		std::string name = "PASS";
-		std::string password;
+		std::string pass;
+		std::string nick;
 		c_irc::User &user = *users[fd];
 
+		nick = user.get_nick();
+		nick = nick.empty() ? "*" : nick;
+
+		LOG_USER(fd, "attempting to register");
 		if (args.size() == 0)
 		{
-			queue_message(ERR_NEEDMOREPARAMS(name), fd);
+			queue_message(ERR_NEEDMOREPARAMS(nick, name), fd);
 			return ;
 		}
 
-		if (not user.get_mode() | U_MODE_RESTRICTED)
+		if (not (user.get_mode() & U_MODE_RESTRICTED))
 		{
-			queue_message(ERR_ALREADYREGISTERED, fd);
+			queue_message(ERR_ALREADYREGISTERED(nick), fd);
 			return ;
 		}
 
-		password = args[0];
+		pass = args[0];
 
-		if (password != server.get_password())
-		{
-			queue_message(ERR_PASSWDMISMATCH, fd);
+		if (pass != get_password()) {
+			LOG_USER(fd, "wrong password");
 			return ;
 		}
+		
+		user.set_mode(user.get_mode() & ~U_MODE_RESTRICTED);
+		LOG_USER(fd, "successfully registered");
 	}
 }
