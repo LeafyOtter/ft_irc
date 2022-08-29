f4fce69dd9b8864a2d8710e7d8679aa326099d3d
loutre <loutre@courrier.dev>
2022-08-29 19:19:47 +0200

    [utils] : added a get_time function

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -1,14 +1,24 @@
-#pragma once
-
 #include <sstream>
 
 namespace c_irc
 {
-	int 	stoi(std::string str)
+	int stoi(std::string str)
 	{
 		int i;
 		std::stringstream ss(str);
 		ss >> i;
 		return (i);
+	}
+
+	std::string get_time()
+	{
+		time_t rawtime;
+		struct tm *timeinfo;
+		std::string ret;
+
+		time(&rawtime);
+		timeinfo = localtime(&rawtime);
+		ret = asctime(timeinfo);
+		return (ret);
 	}
 } // namespace c_irc
